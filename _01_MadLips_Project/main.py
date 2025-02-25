8a7b0e34fc963b4fe9ead520e0e4c6a46e0fb2cf
Rayyan Naveed <rayyannaveed33@gmail.com>
2025-02-25 17:09:20 +0500

    create a mad lips python project
    In this project user inputs some answer to get a prompt of it.

---
+++ _01_MadLips_Project/main.py
@@ -0,0 +1,17 @@
+# Mad Lips Python Project 
+
+your_name = input("Enter your name: ").title()
+hobby = input("Enter your hobby: ").title()
+favorite_food = input("Enter your favorite food: ").title()
+dream_job = input("Enter your dream job: ").title()
+favorite_place = input("Enter your favorite place: ").capitalize()
+best_friend = input("Enter your best friend's name: ").title()
+studying_place = input("Enter your university / college / school / other institute name: ").upper()
+
+print(f"""
+Hey, I am {your_name}, and I have a craze for {hobby} so much! 
+When I'm not busy in {hobby}, you can usually find me eating {favorite_food} 
+or dreaming about becoming a {dream_job}. My favorite place in the world is {favorite_place}, 
+and I love going there with my best friend, {best_friend}. 
+At Last, I am currently studying in {studying_place}.
+""")
