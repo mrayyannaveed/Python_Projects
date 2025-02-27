2f8d3f2cfa907bea7679bf44f8dbe8c8a39aee4f
Rayyan Naveed <rayyannaveed33@gmail.com>
2025-02-27 11:16:32 +0500

    Create a Rock Paper Scissors project
    A classic game where the user plays against the computer.

---
+++ _04_Rock_Papre_Scissors/main.py
@@ -0,0 +1,33 @@
+import random
+
+options:tuple = ("rock", "paper", "scissors")
+playing = True
+
+
+while playing:
+    player = None
+    computer = random.choice(options)
+
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors): ")
+
+    print(f"player: {player}")
+    print(f"computer: {computer}")
+
+    if player == computer:
+        print("It's a tie!")
+    elif player == "rock" and computer == "scissors":
+        print("You win!")
+    elif player == "paper" and computer == "rock":
+        print("You win!")
+    elif player == "scissors" and computer == "paper":
+        print("You win!")
+    else:
+        print("You lose!")
+    
+    play_again = input("Play again? (y/n): ").lower() 
+
+    if not play_again == "y":
+        playing = False
+
+print("Thanks for playing!")
